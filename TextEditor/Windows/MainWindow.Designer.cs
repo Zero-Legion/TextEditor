55e45c8c47b92ce4ae4ca62ec8c345f9786705e5
Zero Legion <Zero Legion@ZEROLEGION>
2023-04-12 19:15:27 +0300

    Add project files.

---
+++ TextEditor/Windows/MainWindow.Designer.cs
@@ -0,0 +1,290 @@
+﻿//-----------------------------------------------------------------------
+
+//-----------------------------------------------------------------------
+namespace TextEditor
+{
+    /// <summary>
+    /// Represents the main window designer class.
+    /// </summary>
+    public partial class MainWindow
+    {
+        /// <summary>
+        /// The menu of the program.
+        /// </summary>
+        private System.Windows.Forms.MenuStrip menu;
+
+        /// <summary>
+        /// The create a new file menu item.
+        /// </summary>
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+
+        /// <summary>
+        /// The open a file menu item.
+        /// </summary>
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+
+        /// <summary>
+        /// The save a file menu item.
+        /// </summary>
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+
+        /// <summary>
+        /// The save a file as menu item.
+        /// </summary>
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+
+        /// <summary>
+        /// The font settings menu item.
+        /// </summary>
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+
+        /// <summary>
+        /// The text box.
+        /// </summary>
+        private System.Windows.Forms.RichTextBox richTextBox1;
+
+        /// <summary>
+        /// The open a file dialog.
+        /// </summary>
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+
+        /// <summary>
+        /// The save a file as dialog.
+        /// </summary>
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+
+        /// <summary>
+        /// The change font dialog.
+        /// </summary>
+        private System.Windows.Forms.FontDialog fontDialog1;
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">True if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (this.components != null))
+            {
+                this.components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.colorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.printPrevizualizareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inchideAplicatieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setarePaginaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.pageSetupDialog1 = new System.Windows.Forms.PageSetupDialog();
+            this.printPreviewDialog2 = new System.Windows.Forms.PrintPreviewDialog();
+            this.menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menu
+            // 
+            this.menu.BackColor = System.Drawing.SystemColors.Control;
+            this.menu.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.settingsToolStripMenuItem,
+            this.colorToolStripMenuItem,
+            this.printToolStripMenuItem,
+            this.printPrevizualizareToolStripMenuItem});
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.Padding = new System.Windows.Forms.Padding(3, 1, 0, 1);
+            this.menu.Size = new System.Drawing.Size(610, 24);
+            this.menu.TabIndex = 0;
+            this.menu.Text = "menu";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.setarePaginaToolStripMenuItem,
+            this.inchideAplicatieToolStripMenuItem});
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(72, 22);
+            this.newToolStripMenuItem.Text = "Fisier Nou";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.NewToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(67, 22);
+            this.openToolStripMenuItem.Text = "Deschide";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(63, 22);
+            this.saveToolStripMenuItem.Text = "Salveaza";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.SaveToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(59, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.SaveAsToolStripMenuItem_Click);
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(78, 22);
+            this.settingsToolStripMenuItem.Text = "Setare Font";
+            this.settingsToolStripMenuItem.Click += new System.EventHandler(this.FontSettingsToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Text files (*.txt)|*.txt|Rich text format (*.rtf)|*.rtf|All files (*.*)|*.*";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "txt";
+            this.saveFileDialog1.Filter = "Text files (*.txt)|*.txt|Rich text format (*.rtf)|*.rtf|All files (*.*)|*.*";
+            // 
+            // fontDialog1
+            // 
+            this.fontDialog1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fontDialog1.Apply += new System.EventHandler(this.fontDialog1_Apply);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.AcceptsTab = true;
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(0, 22);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(610, 315);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            this.richTextBox1.TextChanged += new System.EventHandler(this.TextHasChanged);
+            // 
+            // colorToolStripMenuItem
+            // 
+            this.colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+            this.colorToolStripMenuItem.Size = new System.Drawing.Size(48, 22);
+            this.colorToolStripMenuItem.Text = "Color";
+            this.colorToolStripMenuItem.Click += new System.EventHandler(this.colorToolStripMenuItem_Click);
+            // 
+            // printToolStripMenuItem
+            // 
+            this.printToolStripMenuItem.Name = "printToolStripMenuItem";
+            this.printToolStripMenuItem.Size = new System.Drawing.Size(44, 22);
+            this.printToolStripMenuItem.Text = "Print";
+            this.printToolStripMenuItem.Click += new System.EventHandler(this.printToolStripMenuItem_Click);
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // printPrevizualizareToolStripMenuItem
+            // 
+            this.printPrevizualizareToolStripMenuItem.Name = "printPrevizualizareToolStripMenuItem";
+            this.printPrevizualizareToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.printPrevizualizareToolStripMenuItem.Text = "Print Previzualizare";
+            this.printPrevizualizareToolStripMenuItem.Click += new System.EventHandler(this.printPrevizualizareToolStripMenuItem_Click);
+            // 
+            // inchideAplicatieToolStripMenuItem
+            // 
+            this.inchideAplicatieToolStripMenuItem.Name = "inchideAplicatieToolStripMenuItem";
+            this.inchideAplicatieToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.inchideAplicatieToolStripMenuItem.Text = "Inchide Aplicatie";
+            this.inchideAplicatieToolStripMenuItem.Click += new System.EventHandler(this.inchideAplicatieToolStripMenuItem_Click);
+            // 
+            // setarePaginaToolStripMenuItem
+            // 
+            this.setarePaginaToolStripMenuItem.Name = "setarePaginaToolStripMenuItem";
+            this.setarePaginaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.setarePaginaToolStripMenuItem.Text = "Setare Pagina";
+            this.setarePaginaToolStripMenuItem.Click += new System.EventHandler(this.setarePaginaToolStripMenuItem_Click);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // printPreviewDialog2
+            // 
+            this.printPreviewDialog2.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog2.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog2.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog2.Enabled = true;
+            this.printPreviewDialog2.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog2.Icon")));
+            this.printPreviewDialog2.Name = "printPreviewDialog2";
+            this.printPreviewDialog2.Visible = false;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 337);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menu);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menu;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "MainWindow";
+            this.Text = "TextEditor";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainWindow_Closing);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainWindow_KeyDown);
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.ToolStripMenuItem colorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printToolStripMenuItem;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ToolStripMenuItem inchideAplicatieToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printPrevizualizareToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem setarePaginaToolStripMenuItem;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Windows.Forms.PageSetupDialog pageSetupDialog1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog2;
+    }
+}
---
+++ TextEditor/Windows/MainWindow.cs
@@ -0,0 +1,315 @@
+﻿
+namespace TextEditor
+{
+    using System;
+    using System.Windows.Forms;
+
+   
+    public partial class MainWindow : Form
+    {
+        
+        private bool textChanged;
+
+        
+        private string fileName;
+
+       
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            this.textChanged = false;
+            this.fileName = "Untilted";
+            this.Text = "TextEditor" + " - " + this.fileName;
+
+            this.OpenFileAtStartUp();
+        }
+
+      
+        private void OpenFileAtStartUp()
+        {
+            string[] args = Environment.GetCommandLineArgs();
+
+            if (args != null && args.Length == 2)
+            {
+                this.fileName = args[1];
+                this.OpenFile();
+            }
+        }
+
+        
+        private void MainWindow_KeyDown(object sender, KeyEventArgs k)
+        {
+            if (k.Control && k.Shift && k.KeyCode == Keys.S)
+            {
+                this.SaveAsToolStripMenuItem_Click(this, new EventArgs());
+            }
+            else if (k.Control)
+            {
+                if (k.KeyCode == Keys.N)
+                {
+                    this.NewToolStripMenuItem_Click(this, new EventArgs());
+                }
+                else if (k.KeyCode == Keys.O)
+                {
+                    this.OpenToolStripMenuItem_Click(this, new EventArgs());
+                }
+                else if (k.KeyCode == Keys.S)
+                {
+                    this.SaveToolStripMenuItem_Click(this, new EventArgs());
+                }
+                else if (k.KeyCode == Keys.F)
+                {
+                    this.FontSettingsToolStripMenuItem_Click(this, new EventArgs());
+                }
+               
+                else if (k.KeyCode == Keys.X)
+                {
+                    this.Close();
+                }
+            }
+        }
+
+        
+        private void TextHasChanged(object sender, EventArgs e)
+        {
+            this.textChanged = true;
+        }
+
+        
+        private string GetFileName(string path)
+        {
+            string[] pathArray = path.Split('\\');
+            return pathArray[pathArray.Length - 1];
+        }
+
+       
+        private void OpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult askIfSave = DialogResult.None;
+
+            if (this.textChanged == true)
+            {
+                askIfSave = MessageBox.Show("Vrei sa salvezi ?", "Windows Editor", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
+            }
+
+            if (askIfSave == DialogResult.Yes)
+            {
+                this.SaveFile(false);
+            }
+
+            if (askIfSave != DialogResult.Cancel)
+            {
+                if (this.openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    this.fileName = this.openFileDialog1.FileName;
+
+                    this.OpenFile();
+                }
+            }
+        }
+        private System.Drawing.Printing.PrintDocument printDocument = new System.Drawing.Printing.PrintDocument();
+
+       
+        private void OpenFile()
+        {
+            try
+            {
+                if (this.CheckIfFileIsRtf(this.fileName) == true)
+                {
+                    this.richTextBox1.LoadFile(this.fileName, RichTextBoxStreamType.RichText);
+
+                    // Sets the font of the richtextbox to the font of the .rtf file.
+                    this.richTextBox1.HideSelection = true;
+                    this.richTextBox1.SelectAll();
+                    this.richTextBox1.Font = this.richTextBox1.SelectionFont;
+                    this.richTextBox1.Select(this.richTextBox1.Text.Length, this.richTextBox1.Text.Length);
+                    this.richTextBox1.HideSelection = false;
+                    this.fontDialog1.Font = this.richTextBox1.Font;
+                }
+                else
+                {
+                    this.richTextBox1.LoadFile(this.fileName, RichTextBoxStreamType.PlainText);
+                }
+
+                this.textChanged = false;
+                this.Text = "TextEditor" + " - " + this.GetFileName(this.fileName);
+                this.openFileDialog1.FileName = string.Empty;
+            }
+            catch
+            {
+                MessageBox.Show("A intervenit o eroare, nu s a putut deschide fisierul!", "TextEditor", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+            }
+        }
+
+       
+        private void NewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult askIfSave = DialogResult.None;
+
+            if (this.textChanged == true)
+            {
+                askIfSave = MessageBox.Show("Vrei sa salvezi schimbarile ?", "TextEditor", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
+            }
+
+            if (askIfSave == DialogResult.Yes)
+            {
+                this.SaveFile(false);
+            }
+
+            if (askIfSave != DialogResult.Cancel)
+            {
+                this.fileName = "Untitled";
+                this.Text = "TextEditor" + " - " + this.fileName;
+                this.richTextBox1.Text = string.Empty;
+                this.textChanged = false;
+            }
+        }
+
+       
+        private void SaveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.SaveFile(false);
+        }
+
+         private void SaveFile(bool saveAs)
+        {
+            DialogResult askIfSave = DialogResult.None;
+
+            if (this.fileName == "Untitled" || saveAs == true)
+            {
+                if (this.saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    this.fileName = this.saveFileDialog1.FileName;
+                    this.Text = "TextEditor" + " - " + this.GetFileName(this.fileName);
+                }
+                else
+                {
+                    askIfSave = DialogResult.Cancel;
+                }
+            }
+
+            if (askIfSave != DialogResult.Cancel)
+            {
+                try
+                {
+                    if (this.CheckIfFileIsRtf(this.fileName) == true)
+                    {
+                        this.richTextBox1.SaveFile(this.fileName, RichTextBoxStreamType.RichText);
+                    }
+                    else
+                    {
+                        this.richTextBox1.SaveFile(this.fileName, RichTextBoxStreamType.PlainText);
+                    }
+
+                    this.textChanged = false;
+                }
+                catch
+                {
+                    MessageBox.Show("A intervenit o eroare, nu am reusit sa salvez", "TextEditor", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                }
+            }
+
+            this.saveFileDialog1.FileName = string.Empty;
+        }
+
+   
+        private bool CheckIfFileIsRtf(string path)
+        {
+            string[] pathArray = path.Split('.');
+
+            if (pathArray[pathArray.Length - 1] == "rtf")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+  
+        private void SaveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.SaveFile(true);
+        }
+
+   
+        private void MainWindow_Closing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult askIfSave = DialogResult.None;
+
+            if (this.textChanged == true)
+            {
+                askIfSave = MessageBox.Show("Vrei sa salvezi modificarile ?", "TextEditor", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
+            }
+
+            if (askIfSave == DialogResult.Yes)
+            {
+                this.SaveFile(false);
+            }
+            else if (askIfSave == DialogResult.Cancel)
+            {
+                e.Cancel = true;
+            }
+        }
+
+      
+        private void FontSettingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.fontDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.richTextBox1.Font = this.fontDialog1.Font;
+            }
+        }
+
+    
+        private void printToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            PrintDialog printDialog = new PrintDialog();
+            if (printDialog.ShowDialog() == DialogResult.OK)
+            {
+                // do something with print
+            }
+        }
+
+        private void fontDialog1_Apply(object sender, EventArgs e)
+        {
+
+        }
+
+        private void colorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.ForeColor = colorDialog1.Color;
+            }
+        }
+
+        private void inchideAplicatieToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void setarePaginaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            PageSetupDialog pageSetupDialog = new PageSetupDialog();
+
+            pageSetupDialog.Document = printDocument;
+
+            if (pageSetupDialog.ShowDialog() == DialogResult.OK)
+            {
+                printDocument.DefaultPageSettings = pageSetupDialog.PageSettings;
+                printDocument.PrinterSettings = pageSetupDialog.PrinterSettings;
+            }
+        }
+
+        private void printPrevizualizareToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            printPreviewDialog1.Document = printDocument;
+            printPreviewDialog1.ShowDialog();
+        }
+    }
+}
